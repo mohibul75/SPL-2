c74693fe19d9f2cf04cdff03e110c18880957f1f
purbo75 <43554251+purbo75@users.noreply.github.com>
2020-02-18 02:38:39 +0600

    App.js File

---
+++ Spl/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TabView from './TabView';
+
+
+function App() {
+  return (
+    <div className="App">
+   <TabView/>
+    </div>
+  );
+}
+
+export default App;

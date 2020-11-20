10e51e4c9a373ab9001e7636bd7985cfe26b1abb
purbo75 <43554251+purbo75@users.noreply.github.com>
2020-11-21 03:48:45 +0600

    Add files via upload

---
+++ Spl/app/components/lesson_components/AddLessonElementSudo.js
@@ -0,0 +1,373 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable func-names */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable class-methods-use-this */
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
+import Popup from 'reactjs-popup';
+import { Link } from 'react-router-dom';
+import FormInput from '../common_components/FormInput';
+import Select from '../common_components/SelectInput';
+import PreviewInput from '../common_components/PreviewInput';
+import { editActions } from '../../actions/edit.actions';
+
+class AddLessonElement extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      element_word: '',
+      // eslint-disable-next-line react/no-unused-state
+      element_wordType: '',
+      element_category: '',
+      images: [null],
+      Category: ['Noun', 'Verb', 'Association', 'Activity'],
+      errors: {},
+      pending: false,
+      submitted: false,
+      video: [null],
+      audio: [null]
+    };
+    this.uploadMultipleFiles = this.uploadMultipleFiles.bind(this);
+    this.uploadAudio = this.uploadAudio.bind(this);
+    this.uploadVedio = this.uploadVedio.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.handleSelectChangeForWordType = this.handleSelectChangeForWordType.bind(
+      this
+    );
+    this.handleWordChange = this.handleWordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSelectChange(e) {
+    this.setState({ element_category: e.target.value });
+  }
+
+  // eslint-disable-next-line no-dupe-class-members
+  handleSelectChangeForWordType(e) {
+    this.setState({ element_wordType: e.target.value });
+  }
+
+  handleWordChange(e) {
+    this.setState({ element_word: e.target.value });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.lesson_name);
+    const element = {
+      lesson_name: 'lesson',
+      // eslint-disable-next-line react/destructuring-assignment
+      word: '' || this.state.element_word,
+      wordType: '' || this.state.element_wordType,
+      category: '' || this.state.element_category,
+      images: this.state.images,
+      audio: this.state.audio,
+      video: this.state.video
+    };
+    this.setState({ pending: true });
+    if (element) {
+      this.props.pending(element);
+    }
+  }
+
+  fileObj = [];
+
+  fileArray = [];
+
+  fileObjAudio = [];
+
+  fileArrayAudio = [];
+
+  fileObjVedio = [];
+
+  fileArrayVedio = [];
+
+  // TODO: seperate video, audio and image validation
+  validExtensions = ['.png', '.jpg', '.mp4', '.mp3'];
+
+  hasExtension(fileName, extesions) {
+    // eslint-disable-next-line react/prop-types,react/destructuring-assignment
+    const pattern = `(${extesions.join('|').replace(/\./g, '\\.')})$`;
+    return new RegExp(pattern, 'i').test(fileName);
+  }
+
+  uploadMultipleFiles(e) {
+    /* Get files in array form */
+    const files = Array.from(e.target.files);
+    /* Map each file to a promise that resolves to an array of image URI's */
+    // eslint-disable-next-line promise/catch-or-return
+    Promise.all(
+      files.map(file => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.addEventListener('load', ev => {
+            // eslint-disable-next-line react/prop-types
+            if (!this.hasExtension(file.name, this.validExtensions)) {
+              this.setState({
+                errors: { fileError: 'extension not supported' }
+              });
+              reject(new Error('Extension not supported'));
+            }
+            resolve(ev.target.result);
+          });
+          reader.addEventListener('error', reject);
+          reader.readAsDataURL(file);
+        });
+      })
+    ).then(
+      // eslint-disable-next-line promise/always-return
+      images => {
+        // eslint-disable-next-line promise/always-return
+        const string = images.toString().split(';base64,')[0];
+        console.log(string);
+        this.fileArray.push(images);
+        /* Once all promises are resolved, update state with image URI array */
+        this.setState({ images: this.fileArray }, function() {
+          console.log(this.state.images);
+        });
+      },
+      // eslint-disable-next-line no-shadow
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  uploadAudio(e) {
+    /* Get files in array form */
+    const files = Array.from(e.target.files);
+    /* Map each file to a promise that resolves to an array of image URI's */
+    // eslint-disable-next-line promise/catch-or-return
+    Promise.all(
+      files.map(file => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.addEventListener('load', ev => {
+            // eslint-disable-next-line react/prop-types
+            if (!this.hasExtension(file.name, this.validExtensions)) {
+              this.setState({
+                errors: { fileError: 'extension not supported' }
+              });
+              reject(new Error('Extension not supported'));
+            }
+            resolve(ev.target.result);
+          });
+          reader.addEventListener('error', reject);
+          reader.readAsDataURL(file);
+        });
+      })
+    ).then(
+      // eslint-disable-next-line promise/always-return
+      audio => {
+        // eslint-disable-next-line promise/always-return
+        const string = audio.toString().split(';base64,')[0];
+        console.log(string);
+        this.fileArrayAudio.push(audio);
+        /* Once all promises are resolved, update state with image URI array */
+        this.setState({ audio: this.fileArrayAudio }, function() {
+          console.log(this.state.audio);
+        });
+      },
+      // eslint-disable-next-line no-shadow
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  uploadVedio(e) {
+    /* Get files in array form */
+    const files = Array.from(e.target.files);
+    /* Map each file to a promise that resolves to an array of image URI's */
+    // eslint-disable-next-line promise/catch-or-return
+    Promise.all(
+      files.map(file => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.addEventListener('load', ev => {
+            // eslint-disable-next-line react/prop-types
+            if (!this.hasExtension(file.name, this.validExtensions)) {
+              this.setState({
+                errors: { fileError: 'extension not supported' }
+              });
+              reject(new Error('Extension not supported'));
+            }
+            resolve(ev.target.result);
+          });
+          reader.addEventListener('error', reject);
+          reader.readAsDataURL(file);
+        });
+      })
+    ).then(
+      // eslint-disable-next-line promise/always-return
+      video => {
+        // eslint-disable-next-line promise/always-return
+        const string = video.toString().split(';base64,')[0];
+        console.log(string);
+        this.fileArray.push(video);
+        /* Once all promises are resolved, update state with image URI array */
+        this.setState({ video: this.fileArrayVideo }, function() {
+          console.log(this.state.video);
+        });
+      },
+      // eslint-disable-next-line no-shadow
+      error => {
+        console.error(error);
+      }
+    );
+  }
+
+  render() {
+    const { pending, errors } = this.state;
+    return (
+      <>
+        <div className="title-block">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <h3>Add lesson's Element</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="add-element">
+          <form name="element_form" onSubmit={this.handleSubmit}>
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6">
+                  <Select
+                    title="Category "
+                    name="Category "
+                    options={this.state.Category}
+                    value={this.state.element_categoryy}
+                    placeholder="Select  Category"
+                    handleChange={this.handleSelectChange}
+                  />
+                  <FormInput
+                    label="Type"
+                    labelClassName="col-sm-2 col-form-label"
+                    name="type"
+                    type="text"
+                    value={this.state.element_wordType}
+                    onChange={this.handleSelectChangeForWordType}
+                    placeholder="Enter  word Type"
+                    error={errors.word}
+                    required
+                    className="form-control"
+                    inputContainerClassName="col-sm-10"
+                  />
+                  <FormInput
+                    label="Word"
+                    labelClassName="col-sm-2 col-form-label"
+                    name="word"
+                    type="text"
+                    value={this.state.element_word}
+                    onChange={this.handleWordChange}
+                    placeholder="Enter a word"
+                    error={errors.word}
+                    required
+                    className="form-control"
+                    inputContainerClassName="col-sm-10"
+                  />
+                  <PreviewInput
+                    name="files"
+                    label="Picture"
+                    onChange={this.uploadMultipleFiles}
+                    files={this.fileArray}
+                    error={errors.fileError}
+                  />
+
+                  <PreviewInput
+                    name="audio"
+                    label="Audio"
+                    onChange={this.uploadAudio}
+                    file={this.fileArrayAudio}
+                    erroe={errors.fileError}
+                  />
+
+                  <PreviewInput
+                    name="vedio"
+                    label="Vedio"
+                    onChange={this.uploadVedio}
+                    file={this.fileArrayVedio}
+                    erroe={errors.fileError}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="grouped-button-bottom-bar">
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm">
+                    <div className="btn btn-primary">
+                      <Link to="/">Back </Link>
+                    </div>
+                    <Popup
+                      trigger={
+                        <button className="btn btn-default" type="button">
+                          Cancel
+                        </button>
+                      }
+                      modal
+                    >
+                      {close => (
+                        <div className="warning-modal">
+                          <div>
+                            <h4>Are you Sure?</h4>
+                            <p>
+                              No changes will be saved. please click confirm to
+                              discard all information
+                            </p>
+                            <Link to="/" className="btn btn-default">
+                              Confirm
+                            </Link>
+                            <button
+                              className="btn btn-light"
+                              type="button"
+                              onClick={() => {
+                                console.log('modal closed ');
+                                close();
+                              }}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        </div>
+                      )}
+                    </Popup>
+                    <button type="submit" className="btn btn-primary">
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+function mapState(state) {
+  const { pending } = state.editForm;
+  return { pending };
+}
+
+const actionCreators = {
+  pending: editActions.pending
+};
+
+const connectedElementForm = connect(
+  mapState,
+  actionCreators
+)(AddLessonElement);
+
+// eslint-disable-next-line import/prefer-default-export
+export { connectedElementForm as AddLessonElement };

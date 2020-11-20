cfa67fcac9a55ecabd08e85421b52875223231b9
purbo75 <43554251+purbo75@users.noreply.github.com>
2020-11-21 04:01:54 +0600

    Add files via upload

---
+++ Spl/app/consta/edits.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const editConstants = {
+  CHANGE: 'ADD_CHANGE',
+  PENDING: 'EDIT_FORM_PENDING',
+  SUBMITTED: 'EDIT_FORM_SUCCESS',
+  FAILED: 'EDIT_FORM_FAILED'
+}
